cf8d371f0cfe35598cc3ee355a16d5bc47306cc4
tmgasek <tomaszgasek28@gmail.com>
2022-12-18 11:22:20 +0000

    i spend too much time on this shit

---
+++ .config/nvim/lua/tmg/telescope.lua
@@ -0,0 +1,59 @@
+local status, telescope = pcall(require, "telescope")
+if (not status) then return end
+local actions = require('telescope.actions')
+local builtin = require("telescope.builtin")
+
+local function telescope_buffer_dir()
+    return vim.fn.expand('%:p:h')
+end
+
+local fb_actions = require "telescope".extensions.file_browser.actions
+
+telescope.setup {
+    defaults = {
+        mappings = {
+            n = {
+                ["q"] = actions.close
+            },
+            i = {
+                ['<C-u>'] = false,
+                ['<C-d>'] = false,
+            },
+        },
+    },
+    extensions = {
+        file_browser = {
+            theme = "dropdown",
+            -- disables netrw and use telescope-file-browser in its place
+            initial_mode = "normal",
+            hijack_netrw = true,
+            mappings = {
+                -- your custom insert mode mappings
+                ["i"] = {
+                    ["<C-w>"] = function() vim.cmd('normal vbd') end,
+                },
+                ["n"] = {
+                    -- your custom normal mode mappings
+                    ["N"] = fb_actions.create,
+                    ["-"] = fb_actions.goto_parent_dir,
+                },
+            },
+        },
+    },
+}
+telescope.load_extension("file_browser")
+pcall(require('telescope').load_extension, 'fzf')
+
+
+
+vim.keymap.set("n", "<leader>pv", function()
+    telescope.extensions.file_browser.file_browser({
+        path = "%:p:h",
+        cwd = telescope_buffer_dir(),
+        respect_gitignore = false,
+        hidden = true,
+        grouped = true,
+        initial_mode = "normal",
+        layout_config = { height = 40 }
+    })
+end)
