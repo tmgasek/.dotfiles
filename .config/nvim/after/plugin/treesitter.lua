13d10552dfda73bd515b2df2c6666e2c48d77545
tmgasek <tomaszgasek28@gmail.com>
2022-12-28 12:13:42 +0000

    add indenting (finally fixed this)

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,9 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
     sync_install = false,
-
+    indent = {
+        enable = true
+    },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
